dc82926cd470f72fbfd1b2871ed2aaf553d7ed5d
Supermarcel10 <marcelbarlik@gmail.com>
2025-04-15 13:30:44 +0100

    - Fixed issue where max round file was not generated.

--- src/smt_lib/smt_lib.rs
+++ src/smt_lib/smt_lib.rs
@@ -440,7 +440,7 @@ pub fn generate_smtlib_files() -> Result<(), Box<dyn Error>> {
 
 	for sha_function in [SHA224, SHA256, SHA512] {
 		for collision_type in [Standard, SemiFreeStart, FreeStart] {
-			for rounds in 0..sha_function.max_rounds() {
+			for rounds in 0..=sha_function.max_rounds() {
 				let mut builder = SmtBuilder::new(sha_function, rounds, collision_type)?;
 				builder.brute_force_encoding();
 				builder.to_file(format!("data/{sha_function}_{collision_type}_{rounds}.smt2").into())?;
